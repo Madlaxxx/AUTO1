e18448f1088969962989eef9da1c0f64b180a3ff
Мария <masha2355@gmail.com>
2024-02-11 15:59:30 +1000

    Testng

--- src/test/java/ru/netolgy/CashbackHackServiceTest.java
+++ src/test/java/ru/netolgy/CashbackHackServiceTest.java
@@ -10,6 +10,19 @@ public class CashbackHackServiceTest {
     public void testBoundary() {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
         int amount = 800;
 
         int actual = cashbackHackService.remain(amount);
@@ -18,4 +31,6 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+
+
 }
