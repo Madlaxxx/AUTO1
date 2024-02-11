7c699ac6d0b9c10e41c84198b9353beb1aa6ee01
Мария <masha2355@gmail.com>
2024-02-11 15:54:36 +1000

    Unit4

--- src/test/java/ru/netolgy/CashbackHackServiceTest.java
+++ src/test/java/ru/netolgy/CashbackHackServiceTest.java
@@ -1,20 +1,32 @@
 package ru.netolgy;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
 
     @Test
     public void testBoundary() {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
-        int amount = 800;
+        int amount = 1000;
 
         int actual = cashbackHackService.remain(amount);
 
-        int expected = 200;
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
